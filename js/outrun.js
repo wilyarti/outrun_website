c0f2ae5f824c7c53f01efc80cbc83d4f688197ed
Wilyarti Howard <wilyarti@gmail.com>
2020-06-12 00:17:50 +1000

    Final changes.

--- js/outrun.js
+++ js/outrun.js
@@ -21,15 +21,14 @@ function hideSubMenu() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-
   if (!event.target.matches('.menu')) {
     console.log("Resetting menu.");
-    document.getElementById("dynamicTitle").classList.remove("show");
-    document.getElementById("resumeBox").classList.remove("show-table");
     document.getElementById("masterMenu").classList.add("show");
     document.getElementById("nameBox").classList.add("show");
+    document.getElementById("dropDownMenu").classList.remove("show");
+    document.getElementById("dynamicTitle").classList.remove("show");
+    document.getElementById("resumeBox").classList.remove("show-table");
     document.getElementById("projectBox").classList.remove("show-img");
-
   }
 }
 
@@ -42,6 +41,11 @@ function showResume() {
 
 function showProjects() {
   console.log("Showing projects.");
+  document.getElementById("imgID").src = myImageObjects[imageCount].image;
+  document.getElementById("imgHref").href = myImageObjects[imageCount].url;
+  document.getElementById("dynamicTitle").innerText = myImageObjects[imageCount].title;
+
+  document.getElementById("dynamicTitle").classList.add("show");
   document.getElementById("projectBox").classList.add("show-img");
   hideMenu();
 }
@@ -65,6 +69,6 @@ function nextImage() {
   document.getElementById("dynamicTitle").classList.add("show");
 
   document.getElementById("imgID").src = myImageObjects[imageCount].image;
-  document.getElementById("imgHref").src = myImageObjects[imageCount].url;
+  document.getElementById("imgHref").href = myImageObjects[imageCount].url;
   document.getElementById("dynamicTitle").innerText = myImageObjects[imageCount].title;
 }
