899eca66312cb1222e8007d8a369bbca4cee1184
wilyarti <wilyarti@gmail.com>
2020-06-14 15:30:30 +1000

    Add transitions. Add preload.

--- js/outrun.js
+++ js/outrun.js
@@ -1,10 +1,13 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function openMenu() {
+  // Fade Out
   document.getElementById("masterMenu").classList.remove("show");
-  document.getElementById("dropDownMenu").classList.toggle("show");
-  document.getElementById("resumeBox").classList.remove("show-table");
-  document.getElementById("projectBox").classList.remove("show-img");
+  document.getElementById("masterMenu").classList.remove("fadeIn");
+
+  // Fade In
+  document.getElementById("dropDownMenu").classList.add("show");
+  document.getElementById("dropDownMenu").classList.add("fadeIn");
 
 }
 
@@ -15,20 +18,26 @@ function hideMenu() {
 
 }
 
-function hideSubMenu() {
-  document.getElementById("dropDownMenu").classList.remove("show");
-}
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.menu')) {
     console.log("Resetting menu.");
+    document.getElementById("masterMenu").classList.remove("fadeIn");
+    document.getElementById("dropDownMenu").classList.remove("fadeIn");
+    document.getElementById("dynamicTitle").classList.remove("fadeIn");
+    document.getElementById("resumeBox").classList.remove("fadeIn");
+    document.getElementById("projectBox").classList.remove("fadeIn");
+
     document.getElementById("masterMenu").classList.add("show");
     document.getElementById("nameBox").classList.add("show");
+
     document.getElementById("dropDownMenu").classList.remove("show");
     document.getElementById("dynamicTitle").classList.remove("show");
     document.getElementById("resumeBox").classList.remove("show-table");
     document.getElementById("projectBox").classList.remove("show-img");
+
+
   }
 }
 
@@ -36,10 +45,13 @@ window.onclick = function (event) {
 function showResume() {
   console.log("Showing resume.");
   document.getElementById("resumeBox").classList.add("show-table");
+  document.getElementById("resumeBox").classList.add("fadeIn");
+
   hideMenu();
 }
 
 function showProjects() {
+  hideMenu();
   console.log("Showing projects.");
   document.getElementById("imgID").src = myImageObjects[imageCount].image;
   document.getElementById("imgHref").href = myImageObjects[imageCount].url;
@@ -47,7 +59,8 @@ function showProjects() {
 
   document.getElementById("dynamicTitle").classList.add("show");
   document.getElementById("projectBox").classList.add("show-img");
-  hideMenu();
+  document.getElementById("dynamicTitle").classList.add("fadeIn");
+  document.getElementById("projectBox").classList.add("fadeIn");
 }
 
 const myImageObjects = [
@@ -57,6 +70,7 @@ const myImageObjects = [
   {image: "img/project4.jpg", url: "https://github.com/wilyarti/AirMonitor", title: "AIR MONITOR APP"},
   {image: "img/project5.jpg", url: "https://opens3.net/channel-grapher.html", title: "CHANNEL GRAPHER"},
   {image: "img/project6.jpg", url: "https://www.thingiverse.com/thing:3601839", title: "LORA WEATHER STATION"},
+  {image: "img/project7.jpg", url: "https://wilyarti-howard.net/hexbattle.html", title: "HEX BATTLE GAME"},
 
 ];
 var imageCount = 0;
